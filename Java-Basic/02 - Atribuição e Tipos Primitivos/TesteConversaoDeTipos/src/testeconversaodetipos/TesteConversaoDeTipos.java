5c7112c232bcaae4bcf2ca39a84f72656cb0183d
Ezequiel Bastos <71236667+ezequielbastos@users.noreply.github.com>
2021-03-22 20:17:57 -0300

    Commit correction of scanner errors and string and float values
    Erro linha 26 - Scanner pegando valor do enter e colocando na variável String
    Correção: Criei um imput.nextLine(); para receber o enter e da procedimento ao recebimento do valor da variavel nota3.
    
    Erro 02 - Inconveniência no imput de números reais String utilizando . ponto e float utilizando , virgula.
    Correção: utilizando a lib locale para setar o padrão americano na obtenção de valores reias!

--- Java-Basic/02 - Atribuição e Tipos Primitivos/TesteConversaoDeTipos/src/testeconversaodetipos/TesteConversaoDeTipos.java
+++ Java-Basic/02 - Atribuição e Tipos Primitivos/TesteConversaoDeTipos/src/testeconversaodetipos/TesteConversaoDeTipos.java
@@ -1,5 +1,6 @@
 package testeconversaodetipos;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 /** 
@@ -10,27 +11,38 @@ import java.util.Scanner;
 public class TesteConversaoDeTipos {
     
     public static void main(String[] args) {
-       //A linha a baixo cria o obj imput para fazer get dos imputs e setar nas variaveis!
-    Scanner imput = new Scanner(System.in);
+     
+    //A linha a baixo serve para que não haja erro de . ou , quando usarmos Double ou Float   
+    Locale.setDefault(Locale.US);
     
-    /* Objeto imput2 criado para sulucionar problema do "Float nota4:" sobrepor
-    *  na frente do "String Nota3: " causando erro por não conseguirmos preenche-lo
-    */
-    Scanner imput2 = new Scanner(System.in); 
+    //A linha a baixo cria o obj imput para fazer get dos imputs e setar nas variaveis!
+    Scanner imput = new Scanner(System.in);
     
     System.out.print("String Nota1: ");
         String nota1 = imput.nextLine();
         
     System.out.print("Int Nota2: ");
         int nota2 = imput.nextInt();
-    
-    System.out.print("String Nota3: "); 
-        String nota3 = imput2.nextLine(); //Digitar em Srtig tem que usar o . ex 784.98
-    
+        
+    System.out.print("String Nota3: ");
+        imput.nextLine();  //recebe o enter para que não haja erro de sobreposição da nota4 sobre a 3
+        String nota3 = imput.nextLine();
+           
     System.out.print("Float nota4: ");
-        float nota4 = imput.nextFloat(); //Inserir dado em Float tem que usar a , ex 6214,10
-    
-    
+        float nota4 = imput.nextFloat();
+        
+        imput.close(); //fecha o objeto! para que ele não fique aberto gatando recursos!
+        
+        //  BLOCO PARA VERIFICAR SE O OBJ ESTA CAPITURANDO A ENTRADA DE DADOS CORRETAMENTE!
+        /*
+        System.out.println("Valores sem esta convertido");
+        System.out.println("---------------------------");
+        System.out.println("Nota 1: " + nota1);
+        System.out.println("Nota 2: " + nota2);
+        System.out.println("Nota 3: " + nota3);
+        System.out.println("Nota 4: " + nota4);
+        System.out.println("---------------------------");
+        */
     int notaConvert1 = Integer.parseInt(nota1);
         System.out.println(notaConvert1 + " String convertido com sucesso para Integer");
     
